9beeb4361eb93c85264c48f5d468646481106c4c
mdpatil46 <govindapatil46@gmail.com>
2025-03-11 12:50:56 +0530

    change in navbar resume button

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -16,7 +16,7 @@ function NavBar() {
   }
 
   function Resume (){
-    window.location.href = "https://mdpatil46.github.io/Resume-/"
+    window.location.href = "https://mdpatil46.github.io/Resume-/";
   }
 
   return (
@@ -70,7 +70,7 @@ function NavBar() {
                       <Link to="/Contact">Contact</Link>
                     </li>
                     <li>
-                      <a href="/">
+                    
                         <button
                           type="button"
                           className ="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800"
@@ -78,7 +78,7 @@ function NavBar() {
                         >
                           Resume
                         </button>
-                      </a>
+                     
                     </li>
                   </ul>
                 </div>
